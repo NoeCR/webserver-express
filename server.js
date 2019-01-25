35a655ffe2039eca4931f4a6329b9967cdf3d72d
NoeCR <noecruzrodriguez@gmail.com>
2019-01-25 19:43:36 +0100

    primer commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname + '/public'));
+
+// Express hbs engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res) {
+    res.render('home', {
+        nombre: 'noé cruz rodríguez'
+    });
+});
+app.get('/about', function(req, res) {
+    res.render('about', {
+        nombre: 'noé cruz rodríguez'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+})
